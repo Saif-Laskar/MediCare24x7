67a524e9e76e90c5f11e29f29f0fe88109235e8b
ikramulSoHeL <73246821+ikramul-SoHeL@users.noreply.github.com>
2022-04-25 20:49:50 +0600

    Update admin.py

--- ambulance/admin.py
+++ ambulance/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
 
-admin.site.register(Ambulance)
+class AmbulanceAdmin(admin.ModelAdmin):
+    list_display = ['vehicleNumber', 'city', 'category', 'driverName', 'available']
+    search_fields = ['vehicleNumber',]
+
+
+
+admin.site.register(Ambulance, AmbulanceAdmin)
+admin.site.register(BookAmbulanceModel)
