a190d4ea838ad835f1d8272d58404d4e904d6c9b
ikramulSoHeL <73246821+ikramul-SoHeL@users.noreply.github.com>
2022-04-25 20:54:34 +0600

    Update admin.py

--- ambulance/admin.py
+++ ambulance/admin.py
@@ -5,7 +5,9 @@ class AmbulanceAdmin(admin.ModelAdmin):
     list_display = ['vehicleNumber', 'city', 'category', 'driverName', 'available']
     search_fields = ['vehicleNumber',]
 
-
+class BookAmbulanceModelAdmin(admin.ModelAdmin):
+    list_display = ['ambulance', 'patient', 'date', 'time', 'patientContact']
+    search_fields = ['ambulance', 'patient', 'date', 'time', 'patientContact']
 
 admin.site.register(Ambulance, AmbulanceAdmin)
-admin.site.register(BookAmbulanceModel)
+admin.site.register(BookAmbulanceModel, BookAmbulanceModelAdmin)
