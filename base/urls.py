442422a8b81693e199b3d1c951e978742ecae02b
ikramulSoHeL <73246821+ikramul-SoHeL@users.noreply.github.com>
2022-04-27 04:36:31 +0600

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from base import settings
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
-    path('', include('ambulance.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("accounts.urls")),
+    path("", include("ambulance.urls")),
+    path("pharmacy/", include("pharmacy_control.urls")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
