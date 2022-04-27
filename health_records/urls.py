694857248b5dc01e61dad513d89d14c927dc26de
azizul052 <2018-2-60-052@std.ewubd.edu>
2022-04-27 14:34:48 +0600

    health-record-detail url added

--- health_records/urls.py
+++ health_records/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('<str:pk>', health_record_home_view, name='health-record-home'),
     path('record/new/', health_record_create_view, name='health-record-create'),
+    path('record/<str:pk>/', health_record_detail_view, name='health-record-post-detail'),
 ]
