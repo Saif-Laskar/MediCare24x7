84149e1fcbe759f504f1b8f9c1c615af2ba07bdb
azizul052 <2018-2-60-052@std.ewubd.edu>
2022-04-27 14:44:05 +0600

    health-record forms altered

--- health_records/forms.py
+++ health_records/forms.py
@@ -1,19 +1,15 @@
 from django import forms
-from django.forms import widgets
-from .models import AdviceModel
+from .models import HealthRecordModel
 
 
-class AddEditPostForm(forms.ModelForm):
+class RecordForm(forms.ModelForm):
     """
-    This form is used to add or edit a health advice.
+    This form is used to create a new health record for a patient.
 
-    This form displays a textarea for the user to enter a health advice, along with a title, and an image.
+    This form consists of two fields.
+     - title: The title of the health record.
+     - content: The content of the health record.
     """
-    image = forms.ImageField(required=False,
-                             error_messages={
-                                 "required": "Please upload an image"},
-                             widget=forms.FileInput)
-
     class Meta:
-        model = AdviceModel
-        fields = ['title', 'content', 'image']
+        model = HealthRecordModel
+        fields = ['title', 'content']
