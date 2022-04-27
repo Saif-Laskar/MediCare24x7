b7974ff10565ec86f341be87730b64a8c129c726
oDin-bd <57081378+Saif-Laskar@users.noreply.github.com>
2022-04-27 23:33:59 +0600

    Ambulance All views test done

--- ambulance/tests.py
+++ ambulance/tests.py
@@ -35,5 +35,20 @@ class TestAmbulanceView(TestCase):
         response = self.client.get(reverse('ambulance-booking',args=['1']))
         self.assertEquals(response.status_code,302)
         self.assertRedirects(response, '/accounts/login?next=%2Fambulance-booking%2F1')
+
+    def test_confirm_ambulance_booking_view(self):
+        response = self.client.get(reverse('cinfirm-ambulance-booking',args=['1']))
+        self.assertEquals(response.status_code,302)
+        self.assertRedirects(response, '/accounts/login?next=%2Fcinfirm-ambulance-booking%2F1')
+    
+    def test_staff_available_ambulance_view(self):
+        response = self.client.get(reverse('staff-available-ambulance'))
+        self.assertEquals(response.status_code,302)
+        self.assertRedirects(response, '/accounts/login?next=%2Fstaff-available-ambulance%2F')
+
+    def test_staff_booked_ambulance_view(self):
+        response = self.client.get(reverse('staff-booked-ambulance'))
+        self.assertEquals(response.status_code,302)
+        self.assertRedirects(response, '/accounts/login?next=%2Fstaff-booked-ambulance%2F')
     
     
