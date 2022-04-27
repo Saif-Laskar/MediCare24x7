295819967083a478a83fe2b6b12628584d9077ab
oDin-bd <57081378+Saif-Laskar@users.noreply.github.com>
2022-04-27 23:24:14 +0600

    ambulance testing done

--- ambulance/tests.py
+++ ambulance/tests.py
@@ -8,5 +8,32 @@ class TestAmbulanceView(TestCase):
 
     def test_add_ambulance_view(self):
         response = self.client.get(reverse('add-ambulance'))
-        self.assertEquals(response.status_code,200)
-        self.assertTemplateUsed(response, 'ambulance/add_ambulance.html')
+        self.assertEquals(response.status_code,302)
+        self.assertRedirects(response, '/accounts/login?next=%2Fadd-ambulance%2F')
+
+    def test_edit_ambulance_view(self):
+        response = self.client.get(reverse('edit-ambulance-details', args=['1']))
+        self.assertEquals(response.status_code,302)
+        self.assertRedirects(response, '/accounts/login?next=%2Fambulance%2Fedit-ambulance-details%2F1')
+
+    def test_staff_abmulance_view(self):
+        response = self.client.get(reverse('staff-all-ambulance'))
+        self.assertEquals(response.status_code,302)
+        self.assertRedirects(response, '/accounts/login?next=%2Fstaff-all-ambulance%2F')
+
+    def test_ambulance_detail_view(self):
+        response = self.client.get(reverse('ambulance-details', args=['1']))
+        self.assertEquals(response.status_code,302)
+        self.assertRedirects(response, '/accounts/login?next=%2Fambulance%2Fambulance-details%2F1')
+
+    def test_patient_available_abmulance_view(self):
+        response = self.client.get(reverse('patient-available-abmulance'))
+        self.assertEquals(response.status_code,302)
+        self.assertRedirects(response, '/accounts/login?next=%2Fpatient-available-abmulance%2F')
+    
+    def test_ambulance_booking_view(self):
+        response = self.client.get(reverse('ambulance-booking',args=['1']))
+        self.assertEquals(response.status_code,302)
+        self.assertRedirects(response, '/accounts/login?next=%2Fambulance-booking%2F1')
+    
+    
