663252e6846241406a66495136348f451dadf62c
azizul052 <2018-2-60-052@std.ewubd.edu>
2022-04-27 14:30:25 +0600

    health-record-create url added

--- health_records/urls.py
+++ health_records/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('<str:pk>', health_record_home_view, name='health-record-home'),
-    
+    path('record/new/', health_record_create_view, name='health-record-create'),
 ]
